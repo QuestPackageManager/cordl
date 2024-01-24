c0bc16fea26b2925dae6fc1c1b6377d50477f399
RedBrumbler <redbrumbler.official+github@gmail.com>
2024-01-24 16:38:06 +0100

    Improvements to unityW

--- cordl_internals/exceptions.hpp
+++ cordl_internals/exceptions.hpp
@@ -4,12 +4,16 @@
 
 namespace {
 namespace cordl_internals {
-    struct FieldException : public ::il2cpp_utils::exceptions::StackTraceException {
-        using StackTraceException::StackTraceException;
-    };
+struct FieldException : public ::il2cpp_utils::exceptions::StackTraceException {
+  using StackTraceException::StackTraceException;
+};
 
-    struct NullException : public ::il2cpp_utils::exceptions::StackTraceException {
-        using StackTraceException::StackTraceException;
-    };
-}
+struct NullException : public ::il2cpp_utils::exceptions::StackTraceException {
+  using StackTraceException::StackTraceException;
+};
+
+struct CastException : public ::il2cpp_utils::exceptions::StackTraceException {
+  using StackTraceException::StackTraceException;
+};
+} // namespace cordl_internals
 } // end anonymous namespace
